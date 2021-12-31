e5a8d4f97ba9552449ff316d1751764583d9facb
karahbit <gkoubbe@outlook.com>
2021-12-31 03:34:13 -0500

    CSV parser

--- csv_parser.py
+++ csv_parser.py
@@ -1,81 +1,86 @@
-# Approach 1: Using CSV module
-import csv
-
-print("Using CSV module:")
 line = 'foo, bar, "one, two", three four'
-print(line)
 
-reader = csv.reader([line], skipinitialspace=True)
-res = []
-for r in reader:
-    for i in r:
-        res.append(i)
-print(res)
 
 # Construct the line back
-# line = ", ".join(res)
-line = ""
-for i, string in enumerate(res):
-    if "," in string:
-        line += '"' + string + '"'
-    else:
-        line += string
+def reconstruct_line(res):
+    # line = ", ".join(res)
+    line = ""
+    for i, string in enumerate(res):
+        if "," in string:
+            line += '"' + string + '"'
+        else:
+            line += string
 
-    if i != len(res) - 1:
-        line += ', '
+        if i != len(res) - 1:
+            line += ', '
+    return line
 
+
+# Approach 1: Using CSV module
+print("Using CSV module:")
 print(line)
+
+
+def csv_parser(line):
+    import csv
+
+    reader = csv.reader([line], skipinitialspace=True)
+    res = []
+    for r in reader:
+        for i in r:
+            res.append(i)
+    return res
+
+
+res = csv_parser(line)
+print(res)
+print(reconstruct_line(res))
 
 
 # Approach 2: Replacing all delimiters inside quotes, splitting and then replacing back
 print("\nUsing replace and split:")
 print(line)
 
-in_quote = None
-indexes = []
-for i, letter in enumerate(line):
-    if in_quote:
-        if (letter == in_quote):
-            in_quote = None
+
+def csv_parser(line):
+    in_quote = None
+    indexes = []
+    for i, letter in enumerate(line):
+        if in_quote:
+            if (letter == in_quote):
+                in_quote = None
+            else:
+                if letter == ',':
+                    indexes.append(i)
         else:
-            if letter == ',':
-                indexes.append(i)
-    else:
-        if (letter == '"' or letter == "'"):
-            in_quote = letter
-for i in indexes:
-    line = line[:i] + ';' + line[i+1:]
-print(line)
+            if (letter == '"' or letter == "'"):
+                in_quote = letter
+    for i in indexes:
+        line = line[:i] + ';' + line[i+1:]
+    # print(line)
 
-res = line.split(', ')
+    res = line.split(', ')
+    # print(res)
+
+    for i, string in enumerate(res):
+        if ';' in string:
+            string = string.replace(';', ',')
+            res[i] = string.strip('"')
+    return res
+
+
+res = csv_parser(line)
 print(res)
-
-for i, string in enumerate(res):
-    if ';' in string:
-        string = string.replace(';', ',')
-        res[i] = string.strip('"')
-print(res)
-
-# Construct the line back
-line = ""
-for i, string in enumerate(res):
-    if "," in string:
-        line += '"' + string + '"'
-    else:
-        line += string
-
-    if i != len(res) - 1:
-        line += ', '
-print(line)
+print(reconstruct_line(res))
 
 # Approach 3: Using loops and conditionals
 print("\nUsing loops and conditionals:")
 print(line)
 
 
-def comma_split(string):
+def csv_parser(string):
     last = 0
-    splits = []
+    res = []
     in_quote = None
     for i, letter in enumerate(string):
         if in_quote:
@@ -86,27 +91,15 @@ def comma_split(string):
                 in_quote = letter
 
         if not in_quote and letter == ',':
-            splits.append(string[last:i].strip(' "'))
+            res.append(string[last:i].strip(' "'))
             last = i+1
 
     if last < len(string):
-        splits.append(string[last:].strip(' "'))
+        res.append(string[last:].strip(' "'))
 
-    return splits
+    return res
 
 
-res = comma_split(line)
+res = csv_parser(line)
 print(res)
-
-# Construct the line back
-# line = ", ".join(res)
-line = ""
-for i, string in enumerate(res):
-    if "," in string:
-        line += '"' + string + '"'
-    else:
-        line += string
-
-    if i != len(res) - 1:
-        line += ', '
-print(line)
+print(reconstruct_line(res))
