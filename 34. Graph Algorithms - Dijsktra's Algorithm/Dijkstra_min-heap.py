3489328d83ab7fc7c86dc2098d59c39efbee719c
karahbit <gkoubbe@outlook.com>
2022-01-19 01:22:08 -0500

    Bellman-Ford Algorithm

--- 34. Graph Algorithms - Dijsktra's Algorithm/Dijkstra_min-heap.py
+++ 34. Graph Algorithms - Dijsktra's Algorithm/Dijkstra_min-heap.py
@@ -17,10 +17,24 @@ def dijkstra(edges, start):
             for edge, w in graph[node]:
                 if edge not in visited:
                     heapq.heappush(min_heap, (d + w, edge))
-                    path[edge].append(node)
+                    if path[edge] == []:
+                        path[edge].append(node)
 
     return visited, path
 
 
 print(dijkstra([[2, 1, 1], [2, 3, 1], [3, 4, 1]], 2))
 
+
+edges = [
+    ["A", "B", 2],
+    ["A", "C", 5],
+    ["B", "C", 6],
+    ["B", "D", 1],
+    ["B", "E", 3],
+    ["C", "F", 8],
+    ["D", "E", 4],
+    ["E", "G", 9],
+    ["F", "G", 7],
+]
+print(dijkstra(edges, "A"))
