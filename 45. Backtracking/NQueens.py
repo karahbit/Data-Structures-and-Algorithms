7b58f6fd9ca06d67587e58462452b4fc50761e59
karahbit <gkoubbe@outlook.com>
2022-02-04 18:25:49 -0500

    Backtracking - NQueens

---
+++ 45. Backtracking/NQueens.py
@@ -0,0 +1,61 @@
+class NQueens:
+    def __init__(self, n):
+        self.n = n
+        self.board = [[0 for i in range(n)] for j in range(n)]
+
+    def print_board(self):
+        for i in range(self.n):
+            for j in range(self.n):
+                if self.board[i][j] == 1:
+                    print(" Q ", end='')
+                else:
+                    print(" - ", end='')
+            print()
+
+    def is_place_safe(self, row, col):
+        for j in range(self.n):
+            if self.board[row][j] == 1:
+                return False
+
+        j = col
+        for i in range(row, -1, -1):
+            if i < 0:
+                break
+            if self.board[i][j] == 1:
+                return False
+            j -= 1
+
+        j = col
+        for i in range(row, self.n):
+            if i < 0:
+                break
+            if self.board[i][j] == 1:
+                return False
+            j -= 1
+
+        return True
+
+    def solve(self, col):
+        if col == self.n:
+            return True
+
+        for row in range(self.n):
+            if self.is_place_safe(row, col):
+                self.board[row][col] = 1
+                if self.solve(col + 1):
+                    return True
+                self.board[row][col] = 0
+
+        return False
+
+    def solve_NQueens(self):
+        if self.solve(0):
+            self.print_board()
+        else:
+            print("Solution does not exist")
+
+
+if __name__ == '__main__':
+    n = 4
+    nq = NQueens(n)
+    nq.solve_NQueens()
