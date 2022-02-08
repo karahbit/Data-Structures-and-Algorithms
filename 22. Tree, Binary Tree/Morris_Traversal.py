efcde27416d5fb2c3e318f48f2c92ee37abb618b
karahbit <gkoubbe@outlook.com>
2022-02-07 21:16:31 -0500

    Morris Traversal

--- 22. Tree, Binary Tree/Morris_Traversal.py
+++ 22. Tree, Binary Tree/Morris_Traversal.py
@@ -1,29 +1,42 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution:
-    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        curr = root
-        res = []
-        while curr:
-            if curr.left:
-                pre = curr.left
-                while pre.right and pre.right is not curr:
-                    pre = pre.right
-                if pre.right is curr:
-                    pre.right = None
-                    # res.append(curr.val)    # in-order
-                    curr = curr.right
-                else:
-                    res.append(curr.val)    # pre-order and post-order
-                    pre.right = curr
-                    curr = curr.left
-            else:
-                res.append(curr.val)
-                curr = curr.right
+class TreeNode:
+    def __init__(self, data):
+        self.val = data
+        self.left = None
+        self.right = None
 
-        # return res[::-1]    # post-order
-        return res
+
+def morris_traversal(root):
+    curr = root
+    res = []
+    while curr:
+        if curr.left:
+            pre = curr.left
+            while pre.right and pre.right is not curr:
+                pre = pre.right
+            if pre.right is curr:
+                pre.right = None
+                # res.append(curr.val)    # in-order
+                curr = curr.right
+            else:
+                res.append(curr.val)    # pre-order and post-order
+                pre.right = curr
+                curr = curr.left
+        else:
+            res.append(curr.val)
+            curr = curr.right
+
+    # return res[::-1]    # post-order
+    return res
+
+
+newBT = TreeNode("Drinks")
+leftChild = TreeNode("Hot")
+tea = TreeNode("Tea")
+coffee = TreeNode("Coffee")
+leftChild.left = tea
+leftChild.right = coffee
+rightChild = TreeNode("Cold")
+newBT.left = leftChild
+newBT.right = rightChild
+
+print(morris_traversal(newBT))
